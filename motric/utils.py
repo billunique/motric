5657bf89e83e84fa03915e4d24e30099286d9a7c
Xia Wang <wangxia44@gmail.com>
2017-03-24 14:12:25 +0800

    Judge duplications when registering devices and omit them.

--- motric/utils.py
+++ motric/utils.py
@@ -404,13 +404,18 @@ def device_register(request):
     status = p.get('status')
     device_id = p.pop('device_id') # list
 
+    lds = LabDevice.objects.all()
+    id_list = [e.device_id for e in lds]
+    duplicate = []
     for i in range(len(device_id)):
-        ld = LabDevice(model=model, project=project, owner=owner, label=label, os=os, po_number=po_number, price_cny=price_cn, price_usd=price_us, lab_location=location, status=status, device_id=device_id[i])
-        ld.save()
-        evt = Event(device=ld, event=log_generator(ld.register_date, 'Device registered directly.', operator))
-        evt.save()
-
-    return HttpResponse(data)
+        if device_id[i] not in id_list:  # Looks like it's not a performance-killer.
+            ld = LabDevice(model=model, project=project, owner=owner, label=label, os=os, po_number=po_number, price_cny=price_cn, price_usd=price_us, lab_location=location, status=status, device_id=device_id[i])
+            ld.save()
+            evt = Event(device=ld, event=log_generator(ld.register_date, 'Device registered directly.', operator))
+            evt.save()
+        else:
+            duplicate.append(device_id[i])
+    return HttpResponse(duplicate) # Meaningless, only useful for debug.
 
 
 def syncer(request):
